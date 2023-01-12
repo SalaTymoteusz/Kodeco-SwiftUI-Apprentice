cffdb66527430c75acfeeef04aad1d3e8950bde2
iOS Developer <tymek.pawel@gmail.com>
2023-01-12 20:00:07 +0100

    Lesson 2. Planning a Paged App

---
+++ 01-checking-your-tools/projects/starter/FirstApp/FirstApp/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  FirstApp
+//
+//  Created by Tymoteusz Sala on 11/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            ZStack {
+                Text("Hello, world!")
+                    .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 01-checking-your-tools/projects/starter/FirstApp/FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Tymoteusz Sala on 11/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
