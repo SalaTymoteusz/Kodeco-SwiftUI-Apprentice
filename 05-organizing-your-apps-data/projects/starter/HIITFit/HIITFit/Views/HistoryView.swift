99930cc3a227dd6b241866be26462588a446b0e8
iOS Developer <tymek.pawel@gmail.com>
2023-01-16 12:16:12 +0100

    Lesson 5. Include dev data

--- 05-organizing-your-apps-data/projects/starter/HIITFit/HIITFit/Views/HistoryView.swift
+++ 05-organizing-your-apps-data/projects/starter/HIITFit/HIITFit/Views/HistoryView.swift
@@ -33,45 +33,36 @@
 import SwiftUI
 
 struct HistoryView: View {
-  let today = Date()
-  let yesterday = Date().addingTimeInterval(-86400)
+    let history = HistoryStore()
 
-  let exercises1 = ["Squat", "Step Up", "Burpee", "Sun Salute"]
-  let exercises2 = ["Squat", "Step Up", "Burpee"]
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            // swiftlint:disable:next multiple_closures_with_trailing_closure
+            Button(action: {}) {
+                Image(systemName: "xmark.circle")
+            }
+            .font(.title)
+            .padding(.trailing)
+            VStack {
+                Text("History")
+                    .font(.title)
+                    .padding()
+                Form {
+                    ForEach(history.exerciseDays) { day in
+                        Section {
+                            ForEach(day.exercises, id: \.self) {
+                                Text($0)
+                            }
+                        } header: {
+                            Text(day.date.formatted(as: "MMM d"))
+                                .font(.headline)
+                        }
 
-  var body: some View {
-    ZStack(alignment: .topTrailing) {
-      // swiftlint:disable:next multiple_closures_with_trailing_closure
-      Button(action: {}) {
-        Image(systemName: "xmark.circle")
-      }
-      .font(.title)
-      .padding(.trailing)
-      VStack {
-        Text("History")
-          .font(.title)
-          .padding()
-        Form {
-          Section(
-            header:
-            Text(today.formatted(as: "MMM d"))
-              .font(.headline)) {
-            ForEach(exercises1, id: \.self) { exercise in
-              Text(exercise)
+                    }
+                }
             }
-          }
-          Section(
-            header:
-            Text(yesterday.formatted(as: "MMM d"))
-              .font(.headline)) {
-            ForEach(exercises2, id: \.self) { exercise in
-              Text(exercise)
-            }
-          }
         }
-      }
     }
-  }
 }
 
 struct HistoryView_Previews: PreviewProvider {
