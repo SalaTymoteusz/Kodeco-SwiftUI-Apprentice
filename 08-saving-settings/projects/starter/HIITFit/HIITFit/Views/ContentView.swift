1ea155994e50f8109b4a12ee5c5b65cc88cf0d57
iOS Developer <tymek.pawel@gmail.com>
2023-02-18 17:24:49 +0100

    Leasson 8. Saving Settings

--- 08-saving-settings/projects/starter/HIITFit/HIITFit/Views/ContentView.swift
+++ 08-saving-settings/projects/starter/HIITFit/HIITFit/Views/ContentView.swift
@@ -33,24 +33,32 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var selectedTab = 9
-
-  var body: some View {
-    TabView(selection: $selectedTab) {
-      WelcomeView(selectedTab: $selectedTab)
-        .tag(9)
-      ForEach(0 ..< Exercise.exercises.count) { index in
-        ExerciseView(selectedTab: $selectedTab, index: index)
-          .tag(index)
-      }
+    @SceneStorage("selectedTab") private var selectedTab = 9
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            WelcomeView(selectedTab: $selectedTab)
+                .tag(9)
+            ForEach(0 ..< Exercise.exercises.count) { index in
+                ExerciseView(selectedTab: $selectedTab, index: index)
+                    .tag(index)
+            }
+        }
+        .onAppear {
+            print(
+              FileManager.default.urls(
+                  for: .documentDirectory,
+                  in: .userDomainMask
+              )
+            )
+        }
+        .environmentObject(HistoryStore())
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
     }
-    .environmentObject(HistoryStore())
-    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+    static var previews: some View {
+        ContentView()
+    }
 }
--- 08-saving-settings/projects/starter/HIITFit/HIITFit/Views/RatingView.swift
+++ 08-saving-settings/projects/starter/HIITFit/HIITFit/Views/RatingView.swift
@@ -33,30 +33,69 @@
 import SwiftUI
 
 struct RatingView: View {
-  @Binding var rating: Int
-  let maximumRating = 5
-
-  let onColor = Color.red
-  let offColor = Color.gray
-
-  var body: some View {
-    HStack {
-      ForEach(1 ..< maximumRating + 1) { index in
-        Image(systemName: "waveform.path.ecg")
-          .foregroundColor(
-            index > rating ? offColor : onColor)
-          .onTapGesture {
-            rating = index
-          }
-      }
+    let exerciseIndex: Int
+    @AppStorage("ratings") private var ratings = "0000"
+    @State private var rating = 0
+    let maximumRating = 5
+    
+    let onColor = Color.red
+    let offColor = Color.gray
+    
+    init(exerciseIndex: Int) {
+        self.exerciseIndex = exerciseIndex
+        let desiredLength = Exercise.exercises.count
+        if ratings.count < desiredLength {
+            ratings = ratings.padding(
+                toLength: desiredLength,
+                withPad: "0",
+                startingAt: 0
+            )
+        }
     }
-    .font(.largeTitle)
-  }
+
+    var body: some View {
+        HStack {
+            ForEach(1 ..< maximumRating + 1) { index in
+                Image(systemName: "waveform.path.ecg")
+                    .foregroundColor(
+                        index > rating ? offColor : onColor)
+                    .onTapGesture {
+                        updateRating(index: index)
+                    }
+                    .onAppear {
+                        convertingRating()
+                    }
+                    .onChange(of: ratings) { _ in
+                        convertingRating()
+                    }
+            }
+        }
+        .font(.largeTitle)
+    }
+    
+    private func updateRating(index: Int) {
+        rating = index
+        let index = ratings.index(
+            ratings.startIndex,
+            offsetBy: exerciseIndex)
+        ratings.replaceSubrange(index...index, with: String(rating))
+    }
+    
+    private func convertingRating() {
+        let index = ratings.index(
+            ratings.startIndex,
+            offsetBy: exerciseIndex
+        )
+        let character = ratings[index]
+        rating = character.wholeNumberValue ?? 0
+    }
 }
 
 struct RatingView_Previews: PreviewProvider {
-  static var previews: some View {
-    RatingView(rating: .constant(3))
-      .previewLayout(.sizeThatFits)
-  }
+    @AppStorage("ratings") static var ratings: String?
+    static var previews: some View {
+        ratings = nil
+        return RatingView(exerciseIndex: 0)
+            .previewLayout(.sizeThatFits)
+    }
 }
