3b2531db22bdd2ddadf35b349ed2a42bb5f5ded0
iOS Developer <tymek.pawel@gmail.com>
2023-02-22 11:36:03 +0100

    Lesson 10. Refining Your App

--- 10-refining-your-app/projects/starter/HIITFit/HIITFit/Views/RaisedButton.swift
+++ 10-refining-your-app/projects/starter/HIITFit/HIITFit/Views/RaisedButton.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2021 Razeware LLC
-///
+/// Copyright (c) 2023 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -32,27 +32,53 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @SceneStorage("selectedTab") private var selectedTab = 9
+struct RaisedButton: View {
+    let buttonText: String
+    let action: () -> Void
     
     var body: some View {
-        ZStack {
-            GradientBackground()
-            TabView(selection: $selectedTab) {
-                WelcomeView(selectedTab: $selectedTab)
-                    .tag(9)
-                ForEach(0 ..< Exercise.exercises.count) { index in
-                    ExerciseView(selectedTab: $selectedTab, index: index)
-                        .tag(index)
-                }
-            }
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+        Button {
+            action()
+        } label: {
+            Text(buttonText)
+                .raisedButtonTextStyle()
         }
+        .buttonStyle(RaisedButtonStyle())
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+extension Text {
+    func raisedButtonTextStyle() -> some View {
+        self
+            .font(.body)
+            .fontWeight(.bold)
+    }
+}
+
+struct RaisedButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(maxWidth: .infinity)
+            .padding(.vertical, 12)
+            .background(
+                Capsule()
+                    .foregroundColor(Color("background"))
+                    .shadow(color: Color("drop-shadow"), radius: 4, x: 6, y: 6)
+                    .shadow(color: Color("drop-highlight"), radius: 4, x: -6, y: -6)
+            )
+    }
+}
+
+struct RaisedButton_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ZStack {
+            RaisedButton(buttonText: "Get Started") {
+                print("Hello World")
+            }
+            .buttonStyle(RaisedButtonStyle())
+            .padding(20)
+        }
+        .background(Color("background"))
+        .previewLayout(.sizeThatFits)
     }
 }
--- 10-refining-your-app/projects/starter/HIITFit/HIITFit/Views/RatingView.swift
+++ 10-refining-your-app/projects/starter/HIITFit/HIITFit/Views/RatingView.swift
@@ -72,18 +72,22 @@ struct RatingView: View {
   var body: some View {
     HStack {
       ForEach(1 ..< maximumRating + 1) { index in
-        Image(systemName: "waveform.path.ecg")
-          .foregroundColor(
-            index > rating ? offColor : onColor)
-          .onTapGesture {
+          Button(action: {
             updateRating(index: index)
-          }
+          }, label: {
+            Image(systemName: "waveform.path.ecg")
+              .foregroundColor(
+                index > rating ? offColor : onColor)
+              .font(.body)
+          })
+          .buttonStyle(EmbossedButtonStyle(buttonShape: .round))
           .onChange(of: ratings) { _ in
             convertRating()
           }
           .onAppear {
             convertRating()
           }
+
       }
     }
     .font(.largeTitle)
